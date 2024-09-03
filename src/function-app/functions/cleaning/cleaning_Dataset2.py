ae18a02ad20b4e0f7c35b1bf3df7fff73f8746fb
NoorullahKhan <noorullah.khan@students.mq.edu.au>
2024-09-03 22:16:21 +1000

    #15 Added cleaning script for dataset 2 and cleaned data
    updated the folder locations

---
+++ src/function-app/functions/cleaning/cleaning_Dataset2.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+
+# Get the directory of the current script
+script_dir = os.path.dirname(__file__)
+
+# Construct the relative path to the input dataset
+input_file_path = os.path.join(script_dir, '..', '..', '..', '..', 'assets', 'data', 'raw', 'dataset2.csv')
+
+# Load the dataset
+df = pd.read_csv(input_file_path)
+
+# Filter for motor claims
+df_motor = df.loc[df["INSURANCE_TYPE"] == "Motor"].copy()
+
+# Fill in null values with the lowest degree for EducationLevel
+df_motor['CUSTOMER_EDUCATION_LEVEL'] = df_motor['CUSTOMER_EDUCATION_LEVEL'].fillna('High School')
+
+# Rename the columns for clarity using camelCase
+df_motor.rename(columns={
+    'TENURE': 'timeAsCustomer',
+    'INCIDENT_HOUR_OF_THE_DAY': 'incidentTime',
+    'POLICE_REPORT_AVAILABLE': 'policeReportBool',
+    'AGE': 'driverAge',
+    'CUSTOMER_EDUCATION_LEVEL': 'educationLevel',
+    'PREMIUM_AMOUNT': 'insurancePremium',
+    'INCIDENT_SEVERITY': 'incidentSeverity',
+    'ANY_INJURY': 'numBodilyInjuries',
+    'AUTHORITY_CONTACTED': 'authoritiesInvolved',
+    'CLAIM_AMOUNT': 'totalClaimAmount',
+    'CLAIM_STATUS': 'claimStatus',
+}, inplace=True)
+
+# Construct the path for saving the cleaned output CSV file
+output_file_path = os.path.join(script_dir, '..', '..', '..', '..', 'assets', 'data', 'cleaned', 'cleaned_dataset2.csv')
+
+# Save the cleaned dataframe to the output CSV file
+df_motor.to_csv(output_file_path, index=False)
+
+print("Data cleaning completed. The cleaned dataset has been saved to 'cleaned_dataset2.csv'.")
