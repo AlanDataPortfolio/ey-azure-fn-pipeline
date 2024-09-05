47cbb268c49261e4b305b622aca44b840eb0b20d
aasnayem <adamchowdhury100@gmail.com>
2024-09-05 16:55:16 +1000

    #17 Combined dataset 1 and 2

--- src/function-app/functions/merging/merging_Dataset1&2.py
+++ src/function-app/functions/merging/merging_Dataset1&2.py
@@ -8,4 +8,14 @@ Created on Thu Sep  5 11:15:24 2024
 
 import pandas as pd
 
-df1 = pd.read_csv('')
+# Load dataset 1
+df1 = pd.read_csv('/Users/aasnayemgazzalichowdhury/Desktop/Uni Documents/2024 Session 2/COMP3850/Enriching/cleanedEnriched_Dataset1.csv')
+
+# Load dataset 2
+df2 = pd.read_csv('/Users/aasnayemgazzalichowdhury/Desktop/Uni Documents/2024 Session 2/COMP3850/Enriching/cleanedEnriched_Dataset2.csv')
+
+# Combine the two datasets using concat function
+df_concat = pd.concat([df1, df2])
+
+# Spit out the file
+df_concat.to_csv('/Users/aasnayemgazzalichowdhury/Desktop/Uni Documents/2024 Session 2/COMP3850/Merging/merged_Dataset.csv')
