87687264c70be1abcd45b6dcc70c1a104365c931
NoorullahMuhammadKhan <noorullahkhan@outlook.com>
2024-09-10 11:31:55 +1000

    #28 Deleted redundant folder

--- src/function-app/functions/merging/merging_Datasets_6500_rows.py
+++ src/function-app/functions/merging/merging_Datasets_6500_rows.py
@@ -1,25 +1,35 @@
 import pandas as pd
+import os
 
-# Load dataset 1
-df1 = pd.read_csv('/Users/aasnayemgazzalichowdhury/Desktop/Uni Documents/2024 Session 2/COMP3850/Enriching/cleanedEnriched_Dataset1.csv')
+# Define the base path (working directory)
+base_path = os.getcwd()
 
-# Load dataset 2
-df2 = pd.read_csv('/Users/aasnayemgazzalichowdhury/Desktop/Uni Documents/2024 Session 2/COMP3850/Enriching/cleanedEnriched_Dataset2.csv')
+# Load dataset 1 from 'merged_Dataset.csv'
+df1 = pd.read_csv(os.path.join(base_path, 'ey-azure-fn-pipeline', 'assets', 'data', 'merged', 'merged_Dataset.csv'))
 
-# Load synthesized data
-df3 = pd.read_csv('/Users/aasnayemgazzalichowdhury/Desktop/Uni Documents/2024 Session 2/COMP3850/Synthesizing/SynthesisedMethod1.csv')
+# Load dataset 2 from 'cleanedEnriched_Dataset2.csv'
+df2 = pd.read_csv(os.path.join(base_path, 'ey-azure-fn-pipeline', 'assets', 'data', 'enriched', 'cleanedEnriched_Dataset2.csv'))
 
-# Drop the index column in the synthesized dataset 
+# Load synthesized data from 'SynthesisedMethod1.csv'
+df3 = pd.read_csv(os.path.join(base_path, 'ey-azure-fn-pipeline', 'assets', 'data', 'synthesised', 'SynthesisedMethod1.csv'))
+
+# Drop the 'index' column in the synthesized dataset
 df3 = df3.drop('index', axis=1)
 
-# Combine the two datasets using concat function
+# Combine the three datasets using concat function
 df_concat = pd.concat([df1, df2, df3])
 
 # Set the index to start from 1 and not reset after 1000
 df_concat.index = range(1, len(df_concat) + 1)
 
-#Set index name to 'index'
+# Set index name to 'index'
 df_concat.index.name = 'index'
 
-# Spit out the file
-df_concat.to_csv('/Users/aasnayemgazzalichowdhury/Desktop/Uni Documents/2024 Session 2/COMP3850/Merging/merged_Dataset_6500_rows.csv')
+# Define the path for the output file
+output_file_path = os.path.join(base_path, 'ey-azure-fn-pipeline', 'assets', 'data', 'merged', 'merged6500.csv')
+
+# Save the combined dataset to the specified path
+df_concat.to_csv(output_file_path, index=True)
+
+# Print a statement to indicate the process is done
+print(f"Merged data saved to '{output_file_path}'")
