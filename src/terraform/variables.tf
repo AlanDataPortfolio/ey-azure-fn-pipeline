f6515512a3019f82f9d11fe4fa57f1df4601b60a
Alan Nguyen <alan.ng.code@gmail.com>
2024-10-03 18:26:33 +1000

    #34: Updated versions.tf & variables.tf
    Used variables which will be imported from repo secrets

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -13,3 +13,8 @@ variable "location" {
   description = "The Azure region to deploy to"
   type        = string
 }
+
+variable "azure_subscription_id" {
+  description = "The Azure subscription ID"
+  type        = string
+}
